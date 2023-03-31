be570584f0d213de8720f717aabc701d7255286c
TATABAS1996 <sebastianannett@hotmail.co.uk>
2023-03-31 16:36:41 +0100

    fixed the add and edit showings pages

--- Cinema-System-ESD-Component B/UWEFlix_django/uweflix/views.py
+++ Cinema-System-ESD-Component B/UWEFlix_django/uweflix/views.py
@@ -277,33 +277,49 @@ def add_showing(request):
 
     return render(request, 'uweflix/add_showing.html', context)
 
-
 def edit_showing(request, showing_id):
-    form = editShowingForm()
-    context = {"form":form}
     showing = get_object_or_404(Showing, id=showing_id)
-    if 'edit_showing' in request.POST:
-        screen = request.POST.get("Screen")
-        film = request.POST.get("Film")
-        time = request.POST.get("Time")
-        try:
-            showing = Film.objects.get(id=showing_id)
-            if time is not None:
-                showing.screen = screen
-                showing.film = film
-                showing.time = time
-                if form.is_valid():
-                    form.save()
-                showing.save()
-                messages.success(request, 'Showing updated successfully!')
-            else:
-                messages.error(request, 'Invalid time')
-        except Showing.DoesNotExist:
-                messages.error(request, "Showing doesn't exists")
-    #else:
-        #form = editShowingForm(instance=showing)
-    context['form'] = form
+    form = editShowingForm(instance=showing)
+    context = {"form": form}
+
+    if request.method == "POST":
+        form = editShowingForm(request.POST, instance=showing)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Showing updated successfully!')
+            
+            return redirect('add_showing')  # Replace 'add_showing' with the name of the view you want to redirect to after a successful edit
+        else:
+            messages.error(request, 'Error updating the showing. Please check the form for any mistakes.')
+
     return render(request, 'uweflix/edit_showing.html', context)
+
+# def edit_showing(request, showing_id):
+#     form = editShowingForm()
+#     context = {"form":form}
+#     showing = get_object_or_404(Showing, id=showing_id)
+#     if 'edit_showing' in request.POST:
+#         screen = request.POST.get("Screen")
+#         film = request.POST.get("Film")
+#         time = request.POST.get("Time")
+#         try:
+#             showing = Film.objects.get(id=showing_id)
+#             if time is not None:
+#                 showing.screen = screen
+#                 showing.film = film
+#                 showing.time = time
+#                 if form.is_valid():
+#                     form.save()
+#                 showing.save()
+#                 messages.success(request, 'Showing updated successfully!')
+#             else:
+#                 messages.error(request, 'Invalid time')
+#         except Showing.DoesNotExist:
+#                 messages.error(request, "Showing doesn't exists")
+#     #else:
+#         #form = editShowingForm(instance=showing)
+#     context['form'] = form
+#     return render(request, 'uweflix/edit_showing.html', context)
 
 def delete_showing(request, showing_id):
     if request.method == 'POST':
